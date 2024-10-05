8bb439fc190c08e19a1ecb71941eb88e3f175099
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-05 22:23:20 +0900

    prisma cliantセットアップ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, PrismaModule],
 })
 export class AppModule {}

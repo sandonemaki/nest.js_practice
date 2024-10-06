c24c34dbe664653969b07d3791e1ee42131f2aee
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-06 12:23:15 +0900

    userモデルの作成とリレーション

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ItemsModule, PrismaModule],
+  imports: [ItemsModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
